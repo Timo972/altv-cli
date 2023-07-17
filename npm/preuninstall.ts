c289d4e053f9287efea6764abb7ef0e822064f62
Timo <44401485+Timo972@users.noreply.github.com>
2023-07-17 17:56:50 +0200

    fix: npm install hooks

--- npm/preuninstall.ts
+++ npm/preuninstall.ts
@@ -3,7 +3,7 @@
 import * as fs from "node:fs/promises";
 
 async function uninstall(): Promise<void> {
-  const binaryName = process.platform === "win32" ? "altv-cli.exe" : "altv-cli";
+  const binaryName = process.platform === "win32" ? "altv.exe" : "altv";
 
   await fs.unlink(binaryName);
 }
